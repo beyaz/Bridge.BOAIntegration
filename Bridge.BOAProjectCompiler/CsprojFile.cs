727ee0d999a3678a6d9d0a35bae657415c3fc28b
test <beyaz1404@hotmail.com>
2018-03-15 13:27:05 +0300

    .

---
+++ Bridge.BOAProjectCompiler/CsprojFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.BOAProjectCompiler
+{
+    class CsprojFile
+    {
+
+        public string Write(string path)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(@"");
+        }
+
+    }
+}
