f881f7a2eb37fd96a75b341f93fbcd6af57d4a5a
test <beyaz1404@hotmail.com>
2018-03-19 15:27:06 +0300

    .

---
+++ BOA.One.Office.CardGeneral.DebitCard/BOA.UI.CardGeneral.DebitCard/BrowsePageExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Bridge.BOAIntegration;
+
+namespace BOA.UI.CardGeneral.DebitCard
+{
+    public static class BrowsePageExtensions
+    {
+        #region Public Methods
+        public static void ConfigureColumns(this BrowsePage browseForm, IEnumerable<DataGridColumnInfoContract> columns, bool isSelectionTypeSingle)
+        {
+            var fields = new object[0];
+            foreach (var item in columns)
+            {
+                var field = new object
+                {
+                    ["key"]       = item.BindingPath,
+                    ["name"]      = item.Label,
+                    ["resizable"] = true
+                };
+
+                if (item.DataType?.IsNumeric() == true)
+                {
+                    field["type"]         = "number";
+                    field["numberFormat"] = "M";
+                }
+
+                fields.Push(field);
+            }
+
+            browseForm[AttributeName.DolarColumns] = fields;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        ///     Determines whether this instance is numeric.
+        /// </summary>
+        static bool IsNumeric(this Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (type == typeof(byte) ||
+                type == typeof(sbyte) ||
+                type == typeof(ushort) ||
+                type == typeof(uint) ||
+                type == typeof(ulong) ||
+                type == typeof(short) ||
+                type == typeof(int) ||
+                type == typeof(long) ||
+                type == typeof(decimal) ||
+                type == typeof(double) ||
+                type == typeof(float))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        #endregion
+
+        static class AttributeName
+        {
+            #region Constants
+            internal const string DolarColumns = "$columns";
+            #endregion
+        }
+    }
+}
