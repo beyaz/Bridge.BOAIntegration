350f74a8a0ecc0d76b1c7fc0860df7ca21bdce5b
test <beyaz1404@hotmail.com>
2018-03-20 09:41:43 +0300

    .

--- Bridge.BOAProjectCompiler/BoaXamlToBoaOneXmlConverter.cs
+++ Bridge.BOAProjectCompiler/BoaXamlToBoaOneXmlConverter.cs
@@ -24,7 +24,9 @@ namespace Bridge.BOAProjectCompiler
         #endregion
 
         #region Properties
-        XmlDocument Document => RootNode.OwnerDocument;
+        string      boa_ui_ns => Namespaces.FirstOrDefault(x => x.Value == "clr-namespace:BOA.UI;assembly=BOA.UI").Key;
+        string boa_BusinessComponents_ns => Namespaces.FirstOrDefault(x => x.Value == "clr-namespace:BOA.UI.BusinessComponents;assembly=BOA.UI.BusinessComponents").Key;
+        XmlDocument Document  => RootNode.OwnerDocument;
 
         IDictionary<string, string> Namespaces
         {
@@ -55,54 +57,24 @@ namespace Bridge.BOAProjectCompiler
             ApplyComponentTransforms();
         }
 
-        string boa_ui_ns => Namespaces.FirstOrDefault(x => x.Value == "clr-namespace:BOA.UI;assembly=BOA.UI").Key;
-        
+        static void TransferAttribute(XmlNode xamlNode, string xamlPropertyName, XmlElement newElement, string newAttribute)
+        {
+            if (xamlNode.Attributes?[xamlPropertyName] != null)
+            {
+                newElement.SetAttribute(newAttribute, xamlNode.Attributes?[xamlPropertyName]?.Value);
+            }
+        }
 
+        static void TransferNameAttribute(XmlNode xamlNode, XmlElement newElement)
+        {
+            TransferAttribute(xamlNode, "x:Name", newElement, "x.Name");
+        }
 
         void ApplyComponentTransforms()
         {
             Transform_BMaskedEditorLabeled();
-        }
-        void Transform_BMaskedEditorLabeled()
-        {
-            if (boa_ui_ns == null)
-            {
-                return;
-            }
-
-            Document.GetElementsByTagName(boa_ui_ns + ":" + "BMaskedEditorLabeled").ToList().ForEach(Transform_BMaskedEditorLabeled);
-        }
-
-        
-        void Transform_BMaskedEditorLabeled(XmlNode node)
-        {
-            var newElement = Document.CreateElement("BInputMask");
-
-            var maskValueIsEqualToCardNumber = node.Attributes?["Mask"]?.Value == "#### #### #### ####";
-            if (maskValueIsEqualToCardNumber)
-            {
-                newElement.SetAttribute("type", "CreditCard");
-            }
-            else if (node.Attributes?["Mask"] != null)
-            {
-                newElement.SetAttribute("type", "custom");
-                newElement.SetAttribute("mask", node.Attributes?["Mask"]?.Value);
-            }
-
-            
-            if (node.Attributes?["Text"] != null)
-            {
-                newElement.SetAttribute("value", node.Attributes?["Text"]?.Value);
-            }
-
-            if (node.Attributes?["Label"] != null)
-            {
-                newElement.SetAttribute("hintText", node.Attributes?["Label"]?.Value);
-                newElement.SetAttribute("floatingLabelText", node.Attributes?["Label"]?.Value);
-            }
-
-            node.ParentNode?.InsertBefore(newElement, node);
-            node.ParentNode?.RemoveChild(node);
+            Transform_BDateTimeEditorLabeled();
+            Transform_AccountComponent();
         }
 
         void ApplyLayoutTransforms()
@@ -141,6 +113,104 @@ namespace Bridge.BOAProjectCompiler
             xmlNode.ParentNode.RemoveChild(xmlNode);
         }
 
+        void Transform_BDateTimeEditorLabeled(XmlNode node)
+        {
+            var newElement = Document.CreateElement("BDateTimePicker");
+
+            TransferAttribute(node, "Value", newElement, "value");
+            TransferAttribute(node, "Label", newElement, "floatingLabelTextDate");
+            TransferNameAttribute(node, newElement);
+
+            node.ParentNode?.InsertBefore(newElement, node);
+            node.ParentNode?.RemoveChild(node);
+        }
+
+        void Transform_BMaskedEditorLabeled()
+        {
+            if (boa_ui_ns == null)
+            {
+                return;
+            }
+
+            Document.GetElementsByTagName(boa_ui_ns + ":" + "BMaskedEditorLabeled").ToList().ForEach(Transform_BMaskedEditorLabeled);
+        }
+
+        void Transform_BDateTimeEditorLabeled()
+        {
+            if (boa_ui_ns == null)
+            {
+                return;
+            }
+
+            Document.GetElementsByTagName(boa_ui_ns + ":" + "BDateTimeEditorLabeled").ToList().ForEach(Transform_BMaskedEditorLabeled);
+        }
+
+        void Transform_AccountComponent()
+        {
+            if (boa_BusinessComponents_ns == null)
+            {
+                return;
+            }
+
+            Document.GetElementsByTagName(boa_BusinessComponents_ns + ":" + "AccountComponent").ToList().ForEach(Transform_AccountComponent);
+        }
+
+        void Transform_AccountComponent(XmlNode node)
+        {
+            var newElement = Document.CreateElement("BAccountComponent");
+
+           
+
+            TransferAttribute(node, "AccountNumber", newElement, "accountNumber");
+
+            if (node.Attributes?["VisibilityAccountSuffix"]?.Value == "Collapsed")
+            {
+                newElement.SetAttribute("isVisibleAccountSuffix", "false");
+            }
+
+            TransferAttribute(node, "ShowTaxNumberAndMernisVerifiedDialogMessage", newElement, "showTaxNumberAndMernisVerifiedDialogMessage");
+            TransferAttribute(node, "ShowMernisServiceHealtyDialogMessage", newElement, "showMernisServiceHealtyDialogMessage");
+            TransferAttribute(node, "ShowDialogMessages", newElement, "showDialogMessages");
+            TransferAttribute(node, "ShowCustomerRecordingBranchWarning", newElement, "showCustomerRecordingBranchWarning");
+            TransferAttribute(node, "ShowCustomerBranchAccountMessage", newElement, "showCustomerBranchAccountMessage");
+            TransferAttribute(node, "ShowBlackListDialogMessages", newElement, "showBlackListDialogMessages");
+            TransferAttribute(node, "AllowSharedAccountControl", newElement, "allowSharedAccountControl");
+            TransferAttribute(node, "AllowDoubleSignatureControl", newElement, "allowDoubleSignatureControl");
+            TransferAttribute(node, "Allow18AgeControl", newElement, "allow18AgeControl"); 
+
+
+
+
+            TransferNameAttribute(node, newElement);
+
+            node.ParentNode?.InsertBefore(newElement, node);
+            node.ParentNode?.RemoveChild(node);
+        }
+        void Transform_BMaskedEditorLabeled(XmlNode node)
+        {
+            var newElement = Document.CreateElement("BInputMask");
+
+            var maskValueIsEqualToCardNumber = node.Attributes?["Mask"]?.Value == "#### #### #### ####";
+            if (maskValueIsEqualToCardNumber)
+            {
+                newElement.SetAttribute("type", "CreditCard");
+            }
+            else if (node.Attributes?["Mask"] != null)
+            {
+                newElement.SetAttribute("type", "Custom");
+                newElement.SetAttribute("mask", node.Attributes?["Mask"]?.Value?.Replace("#", "n"));
+            }
+
+            TransferAttribute(node, "Text", newElement, "value");
+            TransferAttribute(node, "Label", newElement, "hintText");
+            TransferAttribute(node, "Label", newElement, "floatingLabelText");
+
+            TransferNameAttribute(node, newElement);
+
+            node.ParentNode?.InsertBefore(newElement, node);
+            node.ParentNode?.RemoveChild(node);
+        }
+
         void TransformStackPanel()
         {
             var nodeList = Document.GetElementsByTagName("StackPanel").ToList();
@@ -150,6 +220,8 @@ namespace Bridge.BOAProjectCompiler
                 OnStackPanel(xmlNode);
             }
         }
+        
+      
         #endregion
     }
 }
