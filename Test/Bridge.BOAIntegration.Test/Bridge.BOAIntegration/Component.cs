d49cdf6a2a37c9e96fb22ebbe80451742a96ba0a
test <beyaz1404@hotmail.com>
2018-03-28 10:39:29 +0300

    .

---
+++ Test/Bridge.BOAIntegration.Test/Bridge.BOAIntegration/Component.cs
@@ -0,0 +1,11 @@
+﻿namespace Bridge.BOAIntegration
+{
+    [External]
+    [Name("React.Component")]
+    public abstract class Component
+    {
+        #region Public Methods
+        public abstract ReactElement render();
+        #endregion
+    }
+}
---
+++ Test/Bridge.BOAIntegration.Test/Bridge.BOAIntegration/ReactDOM.cs
@@ -0,0 +1,13 @@
+﻿using Bridge.Html5;
+
+namespace Bridge.BOAIntegration
+{
+    [External]
+    public class ReactDOM
+    {
+        #region Public Methods
+        [Template("ReactDOM.render({0},{1})")]
+        public static extern object Render(ReactElement reactElement, Element container);
+        #endregion
+    }
+}
