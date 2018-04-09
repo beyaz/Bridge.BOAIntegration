642feeb2dccc84117eed6d5822b73fa8c89a5095
test <beyaz1404@hotmail.com>
2018-04-09 10:33:10 +0300

    .

--- Test/Bridge.BOAIntegration.Test/Bridge.BOAIntegration/ReactUIBuilderTest.Helper.cs
+++ Test/Bridge.BOAIntegration.Test/Bridge.BOAIntegration/ReactUIBuilderTest.Helper.cs
@@ -5,26 +5,31 @@
         #region Methods
         static dynamic BuildUI(string xmlUI, dynamic prop)
         {
+            var componentClassFinder = ComponentClassFinderMethod();
+
             var builder = new ReactUIBuilder
             {
-                ComponentClassFinder = tag =>
-                {
-                    if (tag == nameof(Component_1))
-                    {
-                        return typeof(Component_1);
-                    }
-
-                    return null;
-                },
+                ComponentClassFinder = componentClassFinder,
 
                 XmlUI       = xmlUI,
                 DataContext = prop
-
             };
-
 
             var element = builder.Build();
             return element;
+        }
+
+        static ComponentClassFinder ComponentClassFinderMethod()
+        {
+            return tag =>
+            {
+                if (tag == nameof(Component_1))
+                {
+                    return typeof(Component_1);
+                }
+
+                return null;
+            };
         }
         #endregion
     }
